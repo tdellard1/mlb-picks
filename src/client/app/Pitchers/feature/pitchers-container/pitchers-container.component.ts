1fe14b7287e96b624f9f342a0db1dcf45a7bb0ed
Travis <tdellard1@my.stlcc.edu>
2024-08-04 13:48:13 -0500

    made some updates

--- src/client/app/Pitchers/feature/pitchers-container/pitchers-container.component.ts
+++ src/client/app/Pitchers/feature/pitchers-container/pitchers-container.component.ts
@@ -51,7 +51,11 @@ export class PitchersContainerComponent extends SubscriptionHolder {
               .filter(Boolean)
               .map((pitcherID) => players.find((player: RosterPlayer) => player.playerID === pitcherID)!);
 
-            this.selectedPitcher = this.pitchersArray[0];
+
+
+            const playerID: string = this.route.snapshot.children[0] && this.route.snapshot.children[0].params['playerID'];
+            const selectedPlayer: RosterPlayer | undefined = this.pitchersArray.find((pitcher: RosterPlayer) => pitcher.playerID === playerID);
+            this.selectedPitcher = selectedPlayer || this.pitchersArray[0];
             this.changePitcher();
           }
         })
